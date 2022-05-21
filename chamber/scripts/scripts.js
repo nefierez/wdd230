a4c75efe720be6222037e1c26d69a62f8a071dc8
nefierez <nefierez@byui.edu>
2022-05-21 14:26:50 -0600

    banner

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,18 @@
+let d = new Date().getDay();
+
+const banner = document.getElementById("banner");
+if (d === 1 || d === 7) {
+    banner.style.display = "block";
+}
+
+const close = document.querySelector("#close");
+
+close.addEventListener("click", () => {
+    banner.style.display = "none";
+});
+
+/* ===========================================================================================*/
+
 function toggleMenu() {
     document.querySelector("#navigation").classList.toggle("open");
     document.querySelector("#menu").classList.toggle("open");
@@ -12,17 +27,5 @@ document.querySelector("#date").innerHTML = date;
 document.querySelector("#lastModified").innerHTML = document.lastModified;
 document.querySelector("#year").innerHTML = new Date().getFullYear();
 
-/* ===========================================================================================*/
 
 
-let d = new Date().getDate();
-
-const banner = document.querySelector("#banner")
-if (d === 1 || d === 2) {
-    banner.style.display = "none";
-}
-
-const closeBtn = document.querySelector("#closeBtn");
-closeBtn.addEventListener("#click", () => {
-    banner.style.display = "none";
-});
