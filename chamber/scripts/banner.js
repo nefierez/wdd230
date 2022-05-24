7b1f1fce1671edd59ca16d57ef8a815d3cf626d1
nefierez <nefierez@byui.edu>
2022-05-23 18:12:04 -0600

    menu css

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,1 +1,23 @@
-
+/*
+<header>
+        <picture>
+            <img id="logo" src="images/logo_small.png" alt="Guatemala Chamber of Commerce Logo">
+        </picture>
+        <section id="headings">
+            <h1>Guatemala Chamber <br> of Commerce</h1>
+        </section>
+        <div id="social">
+            <a href="http://twitter.com" target="_blank"><img id="twitter" class="logos" src="images/Twitter_logo.png" alt="Twitter Logo"></a>
+            <a href="http://linkedin.com" target="_blank"><img id="linkedIn" class="logos" src="images/LinkedIn_logo.png" alt="LinkedIn Logo"></a>
+        </div>
+        <nav>
+            <button id="menu"><span>&#9776;</span><span>X</span></button>
+            <ul id="navigation">
+                <li><a href="#">Discover</a></li>
+                <li><a href="#">Directory</a></li>
+                <li><a href="#">Join</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+*/
