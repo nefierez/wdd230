ed53bb354f5d858253367df4ddae42bdb48a0090
nefierez <nefierez@byui.edu>
2022-06-13 22:52:45 -0600

    prophets js

--- week09/scripts/prophets.js
+++ week09/scripts/prophets.js
@@ -1,36 +1,34 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    prophets.forEach(displayProphets);
-  });
+fetch(requestURL) 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const prophets = jsonObject["prophets"];
+        prophets.forEach(displayProphets);
+    });
 
 function displayProphets(prophet) {
-    let card = document.createElement('section');
-    let name = document.createElement('h2');
-    let birthdate = document.createElement('h3');
-    let birthplace = document.createElement('h4');
-    let portrait = document.createElement('img');
-  
+    let card = document.createElement("section");
+    let name = document.createElement("h2");
+    let birth = document.createElement("p");
+    let children = document.createElement("p");
+    let portrait = document.createElement("img");
+
     name.textContent = `${prophet.name} ${prophet.lastname}`;
-  birthdate.textContent = `Born in ${prophet.birthdate}`;
-  birthplace.textContent = `Born at ${prophet.birthplace}`;
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    portrait.setAttribute('loading', 'lazy');
-  
+    birth.textContent = `${prophet.name} was born on ${prophet.birthdate} in ${prophet.birthplace}.`;
+    children.textContent = `He was father of ${prophet.numofchildren} children.`;
+    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute("loading", "lazy");
+
     card.appendChild(name);
-    card.appendChild(birthdate);
-    card.appendChild(birthplace);
+    card.appendChild(birth);
+    card.appendChild(children);
     card.appendChild(portrait);
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
-  }
+    cards.appendChild(card);
+}
+
+document.querySelector("#lastModified").innerHTML = `Last modified on ${document.lastModified}`;
