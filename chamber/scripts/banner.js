75149173c2f3fa8c1ec0bc611f587d9a71958d18
nefierez <nefierez@byui.edu>
2022-06-21 21:55:30 -0600

    js scripts

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -10,36 +10,3 @@ const close = document.querySelector("#close");
 close.addEventListener("click", () => {
     banner.style.display = "none";
 });
-
-
-/*const date = new Date();
-const dayName = date.toLocaleString('en', { weekday: 'long'});
-const dayNumber = date.toLocaleString('en', { day: 'numeric' });
-const month = date.toLocaleString('en', { month: 'long'});
-const year = date.getFullYear();
-
-const fullDate = `Today is ${dayName}, ${month} ${dayNumber}, ${year}`;
-document.querySelector("#date").innerHTML = fullDate;*/
-
-/*
-// Number of days since the user has visted for the discover page
-let lastVisit = document.querySelector('#lastVisit');
-// get the timestamp from the users lastvisit from localStorage
-const lastvisit = Number(localStorage.getItem('lastvisit'));
-// math to convert milliseconds to days
-const FACTOR = 1000 * 60 * 60 * 24;//
-// Subtract the last visit from the current visit or time
-const currentvisit = Date.now() - lastvisit;
-// Math to get the number of days
-const daysbetween = currentvisit / FACTOR;
-const firstMessage = `Welcome back! It has been ${Math.round(daysbetween)} days since your last visit.`
-const repeatMessage = 'Welcome, This is your first visit.'
-// display to the page how many days since the user's last visit
-if (lastvisit !== 0) {
-  lastVisit.textContent = firstMessage;
-} else {
-  lastVisit.textContent = repeatMessage;
-}
-// set the localStorage with a time stamp to the users current visit to the doscover page
-localStorage.setItem('lastvisit', Date.now())
-*/
--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -20,32 +20,3 @@ document.querySelector("#date").innerHTML = fullDate;
 
 document.querySelector("#lastModified").innerHTML = document.lastModified;
 document.querySelector("#year").innerHTML = new Date().getFullYear();
-
-/* ===========================================================================================*/
-
-const API = "7e20a56af3f2f5b94bf18ff525aa7576";
-
-const fetchData = position => {
-  const {latitude, longitude} = position.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${latitude}&lon=${longitude}&appid=${API}`)
-        .then(response => response.json())
-        .then(data => setWeatherData(data));
-}
-
-const setWeatherData = data => {
-  console.log(data);
-  const weatherData = {
-    temperature: data.main.temp,
-    speed: data.wind.speed,
-  }
-  Object.keys(weatherData).forEach(key => {
-    document.getElementById(key).textContent = weatherData[key];
-});
-}
-
-const onLoad = () => {
-  navigator.geolocation.getCurrentPosition(fetchData);
-}
-
-/* ===========================================================================================*/
-
