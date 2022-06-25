5126dc448ef2d7692a80adefacd58a89d7b274f8
nefierez <nefierez@byui.edu>
2022-06-25 15:38:24 -0600

    weather js code

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -5,6 +5,34 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?id=${3598132}&units=imper
     .then((data) => {
 
     const description = data.weather[0].description.toUpperCase();
+    const temp = data.main.temp.toFixed(0);
+    const speed = data.wind.speed;
+    
+    weatherIcon.setAttribute('src', `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+    weatherIcon.setAttribute('alt', description);
+
+    document.querySelector("#currentTemperature").innerHTML = temp;
+    document.querySelector("#currentSpeed").innerHTML = speed;
+    document.querySelector("#weatherDescription").innerHTML = description;
+
+
+    if (temp <= 30 && speed > 3) {
+        const wc = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16))).toFixed(2);
+        document.querySelector("#windChill").innerHTML = wc;
+    } else {
+        document.querySelector("#windChill").innerHTML = "N/A"
+    }
+});
+
+
+/*
+const weatherAPI = "7e20a56af3f2f5b94bf18ff525aa7576";
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?id=${3598132}&units=imperial&lat=&lon=&appid=${weatherAPI}`)
+    .then((response) => response.json())
+    .then((data) => {
+
+    const description = data.weather[0].description.toUpperCase();
 
     currentTemperature.innerHTML = `${data.main.temp.toFixed(0)}`;
     currentSpeed.innerHTML = `${data.wind.speed}`;   
@@ -23,3 +51,4 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?id=${3598132}&units=imper
         windChill.innerHTML = "N/A"
     }
 });
+*/
