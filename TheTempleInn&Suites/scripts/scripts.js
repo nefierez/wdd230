05e5fd8c93e14c4a96b5ef470c3bb02675366757
nefierez <nefierez@byui.edu>
2022-07-09 23:42:40 -0600

    ham Menu

--- TheTempleInn&Suites/scripts/scripts.js
+++ TheTempleInn&Suites/scripts/scripts.js
@@ -1,7 +1,17 @@
 /* ===== Hamburguer Menu =====*/
 function toggleMenu() {
-    document.querySelector("#hamIcon").classList.toggle("open");
-    document.querySelector("#menu").classList.toggle("open");
+    document.querySelector("#navigation").classList.toggle("navigationDisplay");
+    document.querySelector("#closeIcon").classList.toggle("closeIconDisplay");
+    document.querySelector("#hamIcon").classList.toggle("hamIconHide")
 }
 const x = document.querySelector("#menu");
 x.onclick = toggleMenu;
+
+
+/* 
+                        <ul>
+                            <li><a href="reception"></a>Reception</li>
+                            <li><a href="missionaries"></a>Full-time Temple Missionary</li>
+                        </ul>
+
+                        */
