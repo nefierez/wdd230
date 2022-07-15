b7a0c779dc52d05b45c95e4ec3637bdd43d996d6
nefierez <nefierez@byui.edu>
2022-07-14 18:55:16 -0600

    JSON temples page

--- TheTempleInn&Suites/scripts/scripts.js
+++ TheTempleInn&Suites/scripts/scripts.js
@@ -19,3 +19,57 @@ subMenu.onclick = subMenuFunction;
 /*===== Footer =====*/
 document.querySelector("#lastModified").innerHTML = document.lastModified;
 document.querySelector("#year").innerHTML = new Date().getFullYear();
+
+
+
+
+/*===== Footer =====*/
+const URLrequest = 'https://nefierez.github.io/wdd230/TheTempleInn&Suites/scripts/temples.json';
+const infoCards = document.querySelector(".TemplesDedicated");
+
+fetch(URLrequest)
+.then(function (response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+    const temple = jsonObject["temples"];
+    temple.forEach(displayTemple);
+});
+
+function displayTemple(temple) {
+
+    let templeCard = document.createElement("section");
+    let templePhoto = document.createElement("img");
+    let name = document.createElement("h2");
+    let address = document.createElement("p");
+    let phone = document.createElement("p");
+    let email = document.createElement("p");
+    let services = document.createElement("li");
+    let membership = document.createElement("p");
+
+    templePhoto.setAttribute('src', temple.templePhoto);
+    templePhoto.setAttribute('alt', `${temple.name}'s logo`);
+    name.textContent = `${temple.name}`;
+    address.textContent = `${temple.address}`;
+    phone.textContent = `${temple.phone}`;
+    email.textContent = `${temple.email}`;
+    services.innerHTML = `${temple.services}`;
+    membership.textContent = `${temple.membership}`;
+
+    templePhoto.classList.add("templePhoto");
+    name.classList.add("name");
+    address.classList.add("address");
+    phone.classList.add("phone");
+    email.classList.add("email");
+    services.classList.add("services");
+    membership.classList.add("membership");
+
+    templeCard.appendChild(templePhoto);
+    templeCard.appendChild(name);
+    templeCard.appendChild(address);
+    templeCard.appendChild(phone);
+    templeCard.appendChild(email);
+    templeCard.appendChild(services);
+    templeCard.appendChild(membership);
+    infoCards.appendChild(templeCard);
+}
