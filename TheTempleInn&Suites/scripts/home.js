a6e9a1cda30ae1c5746e099b0c6d37818c0e383d
nefierez <nefierez@byui.edu>
2022-07-16 17:07:11 -0600

    current weather

---
+++ TheTempleInn&Suites/scripts/home.js
@@ -0,0 +1,21 @@
+/* ===== Weather =====*/
+const weatherAPI = "7e20a56af3f2f5b94bf18ff525aa7576";
+
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=14.6349&lon=90.5069&units=metric&appid=${weatherAPI}`)
+    .then((weatherURL) => weatherURL.json())
+    .then((data) => {
+        console.log(data)
+
+        const temp = data.current.temp.toFixed(0);
+        const description = data.current.weather[0].description.toUpperCase();
+        const humidity = data.current.humidity;
+        
+        weatherIcon.setAttribute('src', `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`);
+        weatherIcon.setAttribute('alt', description);
+        document.querySelector("#currentTemperature").innerHTML = temp;
+        document.querySelector("#currentHumidity").innerHTML = humidity;
+        document.querySelector("#weatherDescription").innerHTML = description;
+
+
+});
+
--- TheTempleInn&Suites/scripts/scripts.js
+++ TheTempleInn&Suites/scripts/scripts.js
@@ -19,3 +19,25 @@ subMenu.onclick = subMenuFunction;
 /*===== Footer =====*/
 document.querySelector("#lastModified").innerHTML = document.lastModified;
 document.querySelector("#year").innerHTML = new Date().getFullYear();
+
+
+/* ===== Three-days Forecast =====*/
+const API = "7e20a56af3f2f5b94bf18ff525aa7576";
+
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=14.6349&lon=90.5069&units=metric&appid=${API}`)
+    .then((weatherURL) => weatherURL.json())
+    .then((data) => {
+        console.log(data)
+
+        let minTemp1 = data.daily[1].temp.min.toFixed(0);
+        let maxTemp1 = data.daily[1].temp.max.toFixed(0);
+        let description1 = data.daily[1].weather[0].description.toUpperCase();
+        
+        weatherIcon1.setAttribute('src', `https://openweathermap.org/img/w/${data.daily[1].weather[0].icon}.png`);
+        weatherIcon1.setAttribute('alt', description1);
+        document.querySelector(".highest").innerHTML = minTemp1;
+        document.querySelector(".lowest").innerHTML = maxTemp1;
+        document.querySelector(".weatherDescription").innerHTML = description1;
+});
+
+/* ===== Current Date =====*/
