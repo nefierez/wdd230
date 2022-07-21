d2076a073b578f08a7d61ada025c201c04e3b5ac
nefierez <nefierez@byui.edu>
2022-07-20 23:11:13 -0600

    home script changes

--- TheTempleInn&Suites/scripts/home.js
+++ TheTempleInn&Suites/scripts/home.js
@@ -15,3 +15,55 @@ fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=14.6349&lon=90.5069&u
         document.querySelector("#currentHumidity").innerHTML = humidity;
         document.querySelector("#weatherDescription").innerHTML = description;
 });
+
+/* ===== Three-days Forecast =====*/
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=14.6349&lon=90.5069&units=metric&appid=${weatherAPI}`)
+    .then((weatherURL) => weatherURL.json())
+    .then((data) => {
+
+        let minTemp1 = data.daily[1].temp.min.toFixed(1);
+        let maxTemp1 = data.daily[1].temp.max.toFixed(1);
+        let description1 = data.daily[1].weather[0].description.toUpperCase();
+        
+        weatherIcon1.setAttribute('src', `https://openweathermap.org/img/w/${data.daily[1].weather[0].icon}.png`);
+        weatherIcon1.setAttribute('alt', description1);
+        document.querySelector("#highest1").innerHTML = minTemp1;
+        document.querySelector("#lowest1").innerHTML = maxTemp1;
+        document.querySelector("#weatherDescription1").innerHTML = description1;
+
+        let minTemp2 = data.daily[2].temp.min.toFixed(1);
+        let maxTemp2 = data.daily[2].temp.max.toFixed(1);
+        let description2 = data.daily[2].weather[0].description.toUpperCase();
+        
+        weatherIcon2.setAttribute('src', `https://openweathermap.org/img/w/${data.daily[2].weather[0].icon}.png`);
+        weatherIcon2.setAttribute('alt', description2);
+        document.querySelector("#highest2").innerHTML = minTemp2;
+        document.querySelector("#lowest2").innerHTML = maxTemp2;
+        document.querySelector("#weatherDescription2").innerHTML = description2;
+
+        let minTemp3 = data.daily[3].temp.min.toFixed(1);
+        let maxTemp3 = data.daily[3].temp.max.toFixed(1);
+        let description3 = data.daily[3].weather[0].description.toUpperCase();
+        
+        weatherIcon3.setAttribute('src', `https://openweathermap.org/img/w/${data.daily[3].weather[0].icon}.png`);
+        weatherIcon3.setAttribute('alt', description3);
+        document.querySelector("#highest3").innerHTML = minTemp3;
+        document.querySelector("#lowest3").innerHTML = maxTemp3;
+        document.querySelector("#weatherDescription3").innerHTML = description3;
+
+        document.querySelector("#day1").innerHTML = weekDay[threeDays(1)];
+        document.querySelector("#day2").innerHTML = weekDay[threeDays(2)];
+        document.querySelector("#day3").innerHTML = weekDay[threeDays(3)];
+});
+
+/* ===== Three Days Calculation For Forecast =====*/
+const date = new Date();
+const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function threeDays(day) {
+    if (day + date.getDay() > 6) {
+        return day + date.getDay() - 7;
+    }
+    else {
+        return day + date.getDay();
+    }
+}
